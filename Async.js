7d65a95a60badd5d9b6272ebd8f3053d83598749
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-23 20:17:01 +0530

    Async functions

---
+++ Async.js
@@ -0,0 +1,16 @@
+
+
+var get_data = async function(data){
+    console.log("Fetching data")
+    console.log(data)
+    for(var i=0; i<1000; i++){
+        var a
+    }
+    
+}
+
+
+get_data("First value").then((val)=>{
+    get_data("second value")
+})
+console.log("Done with fetchinf")
--- filesysAsync.js
+++ filesysAsync.js
@@ -5,5 +5,18 @@ readFile("./test1.txt", 'utf8', (err, result)=>{
         console.log("Error")
         return
     }
-    console.log(result)
+    res = result
+    console.log("Doing first read")
+
+    readFile("./test.txt", 'utf8', (err, result)=>{
+        if(err){
+            console.log("Error")
+            return
+        }
+        res2 = result
+        console.log("Doing second read")
+    })
 })
+
+console.log("Done with task")
+console.log("Waiting for new task")
