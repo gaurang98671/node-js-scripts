d80241bec4f6dec0ec24258007320bf15dce2b54
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-24 03:17:24 +0530

    callbacks and event loops

---
+++ callback.js
@@ -0,0 +1,22 @@
+const test_callback = function(value, callback) {
+    console.log("Doing something")
+    
+    callback(value)
+    
+}
+
+const foo = async ()=>{console.log("foo")}
+const bar = async ()=>console.log("bar")
+const baz = async ()=>console.log("baz")
+
+
+
+await test_callback(2, val=>{
+    setTimeout(bar,10000)
+    
+    console.log("calling callback function")
+    console.log(val)
+})
+
+
+foo()
