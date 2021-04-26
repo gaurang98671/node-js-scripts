f1b8537ae714dd16235bd31117705a59d34f5437
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-27 02:46:02 +0530

    serving static content with express

--- express/app.js
+++ express/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 
+app.use(express.static('./nav-bar'))
 //app.get
 app.get('/', (req, res)=>{
-    res.end("Home page")
+    res.sendFile(path.resolve(__dirname, './nav-bar/index.html'))
 })
 
 app.get("/about", (req, res)=>{
