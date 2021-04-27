275b63ed79f267105f8e48feb8e52eeb8c60cce7
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-04-27 20:59:40 +0530

    json, query params

---
+++ express/express-json.js
@@ -0,0 +1,34 @@
+const { json } = require('express');
+const express = require('express')
+
+const app = express();
+var arr =[{
+    name: 'gaurang',
+    lastName : 'pawar'
+},
+{
+    name: 'abc',
+    lastName: 'xyz'
+}
+]
+app.get('/',(req, res)=>{
+    res.send("<html><a href='/users'>Users</a></html>")
+})
+
+app.get('/map/users', (req, res)=>{
+    const newUsers = arr.map((product)=>{
+        const {name} = product
+        return {name}
+    })
+    res.json(newUsers)
+})
+
+app.get('/find/user/:userName', (req, res)=>{
+    const {userName} = req.params
+    const user = arr.find((user)=> user.name=== userName)
+    res.json(user)
+})
+
+app.listen(5000, ()=>{
+    console.log("Listening on 5000...")
+})
